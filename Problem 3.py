7a0fd7e560e0883332c701c4fa7b618e94b6c735
Ryan Curcio <ryancurcio@rocketmail.com>
2013-01-17 13:31:40 -0800

    Problem 3 revised

--- Problem 3.py
+++ Problem 3.py
@@ -1,44 +1,32 @@
 
-DEBUG = False
-
-import sys
+DEBUG = True
 
 def getLargeFactors():
-	factor = 2
-	print('What number would you like to find the largest prime factor?')
-	NUMBER = int(input())
+	
+	NUMBER = 6008514
+	
 	largestFactor = 1
-	answer = NUMBER
-	while answer - factor >= 0:
-		# dividing our number by a large factor.
-		answer = answer / factor
 
-		if answer - round(answer) == 0:
-			# To see if the number = 0, if it does, then it is a factor.
-			largestFactor = factor
+	answer = NUMBER
+
+	for i in xrange(2, NUMBER/2):
+		while answer % i == 0:
+
+			answer = answer / i
 			if DEBUG:
-				print(largestFactor)
-				print('^^^^ largestFactor')
-		else:
-			answer *= factor
-			factor += 1
-			if DEBUG:
-				print(answer)
-				print('^^^^ answer')
-				print(factor)
-				print('^^^^ factor')
+				print('answer = %s' % (answer))
+
+			if answer - round(answer) == 0:
+
+				largestFactor = i
+
+			else:
+				answer *= i
+				break
 
 	return largestFactor  
 
 
 
-def playAgain():
-	# Ask if they would like to try again.
-	print('Would you like to play again? Yes or No')
-	return input().lower().startswith('y')
-
-while True:
-	print(getLargeFactors())
-	if not playAgain():
-		sys.exit()
+print(getLargeFactors())
 		
